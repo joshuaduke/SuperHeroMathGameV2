c62c32e2225ab2cd6b81188c31a278a0184e950c
Joshua Duke <dukejoshuae@gmail.com>
2021-09-30 20:57:08 -0400

    Added user info modal on game page, js pseudo code

---
+++ script/game.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+function main(){
+    //display modal where user can enter name
+    //3-2-1 countdown in center page
+    //level 1 starts
+    //display equation and results on logos
+    //game timer starts
+    //images move independently
+    //if correct answer display new equation
+    //if answer not right add to misses
+    //set clicked logo to hidden
+
+    //when timer runs out
+    //level 2 begins
+    //equation and logos are updated
+    //hidden logos are set to visible
+    
+
+
+
+}
