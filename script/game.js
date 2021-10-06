af75cbc9ff9bd35d17a6d1bf85ba38a592e5815f
Joshua Duke <dukejoshuae@gmail.com>
2021-10-05 21:13:05 -0400

    Added countdown

--- script/game.js
+++ script/game.js
@@ -1,13 +1,63 @@
+
+// retrieve user name value
+let userName = document.getElementById(`userName`);
+let modalErrorMsg = document.querySelector(`.errorMsg`);
+
+
+//Details Modal
+//display modal where user can enter name
 let playBtn = document.querySelector(`.playBtn`);
 let detailsModal = document.querySelector(`.userDetailsModal`);
 
 playBtn.addEventListener("click", () => {
-    detailsModal.style.display = "none";
+    if(userName.value == ""){
+        modalErrorMsg.innerHTML = "Please Enter a User Name"
+    } else {
+        alert(userName.value);
+        detailsModal.style.display = "none";
+    }
+    
 })
 
 
+function getRandomNum(){
+    return Math.floor(Math.random() * 12);
+}
+
+function createEquation(){
+    let firstNum = getRandomNum();
+    let secondNum = getRandomNum();
+    let equationBox = document.querySelector(`.equation`);
+    let result;
+
+    equationBox.innerHTML = `${firstNum} + ${secondNum}`;
+    
+    return result = firstNum + secondNum;
+}
+
+function runCountdown(){
+    let counter = document.querySelector(`.countdown`);
+    let timeleft = 3;
+
+    let downloadTimer = setInterval(function(){
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        counter.innerHTML = "Finished";
+    } else {
+        counter.innerHTML = timeleft;
+    }
+    timeleft -= 1;
+    }, 1000);
+    
+}
+
+function playLevelOne(){
+    //level one = addition
+
+}
+
 function main(){
-    //display modal where user can enter name
+
     //3-2-1 countdown in center page
     //level 1 starts
     //display equation and results on logos
@@ -22,7 +72,9 @@ function main(){
     //equation and logos are updated
     //hidden logos are set to visible
 
-
-
+    createEquation();
+    runCountdown()
 
 }
+
+main();
